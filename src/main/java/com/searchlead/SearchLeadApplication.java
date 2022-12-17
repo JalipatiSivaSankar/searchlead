723819d6ed6a186b47f880185e663b88923ea060
jalipatisivasankar <sivasankarjalipati@gmail.com>
2022-12-17 17:02:34 +0530

    search lead backup

---
+++ src/main/java/com/searchlead/SearchLeadApplication.java
@@ -0,0 +1,13 @@
+package com.searchlead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchLeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchLeadApplication.class, args);
+	}
+
+}
